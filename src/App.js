6f9c4f88343e450cc1223d5408c6e5c1c05b2c59
Nicholas <nicxzmiller2@gmail.com>
2020-06-15 07:27:38 +0200

    fixed filtering issue

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import {robots} from "./robots";
 import SearchBox from "./SearchBox";
 
-const App = () =>{
-    return(
-        <div className="tc">
-            <h1 className="f1">ROBOFRIENDS</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div>
-    )
-}
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearch = (e) => {
+        this.setState({searchfield: e.target.value})
+    };
+        render() {
+            const filteredRobots = this.state.robots.filter(robot => {
+                return robot.username.toLowerCase().includes(this.state.searchfield.toLowerCase());
+            })
+            return(
+                <div className="tc">
+                    <h1 className="f1">ROBOFRIENDS</h1>
+                    <SearchBox searchChange={this.onSearch}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
+    }
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Card = ({name, email, id}) => {
+const Card = ({name, email, id, username}) => {
     return(
         <div className="bg-light-green pa3 dib br4 ma2 grow bw2 shadow-5 tc ">
             <img src={`https://robohash.org/${id}?200x200`} alt="robot"/>
             <div>
                 <h2>{name}</h2>
+                <p>{username}</p>
                 <p>{email}</p>
             </div>
         </div>
