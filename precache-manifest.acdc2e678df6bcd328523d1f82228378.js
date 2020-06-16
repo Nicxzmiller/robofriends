7ec63efc4c537189fe03690dae8a1b98a9ad7fcd
Nicholas <nicxzmiller2@gmail.com>
2020-06-16 04:46:55 +0200

    Updates

---
+++ precache-manifest.acdc2e678df6bcd328523d1f82228378.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed911e87438e241a7e0fe95d57461723",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2f94b0cdd05aa9ceabf3",
+    "url": "/robofriends/static/css/main.996121dc.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f94b0cdd05aa9ceabf3",
+    "url": "/robofriends/static/js/main.7a4efed2.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
